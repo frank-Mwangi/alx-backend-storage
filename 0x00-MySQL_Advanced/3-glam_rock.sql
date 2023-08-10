e8b51a427ed029f2bb8f4f71fe40cf65360da4ec
frank-Mwangi <kungofrank@gmail.com>
2023-08-10 18:35:10 +0000

    Glam rock bands by lifespan

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,8 @@
+-- List Glam rock bands by longevity
+SELECT band_name,
+	CASE WHEN split IS NULL THEN 2022 - formed
+		ELSE split -formed END AS lifespan
+FROM metal_bands
+WHERE style="Glam rock"
+ORDER BY lifespan DESC;
+
