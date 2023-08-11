b046c5773928d58a37a51cf985fdfa08f7122a76
frank-Mwangi <kungofrank@gmail.com>
2023-08-11 08:07:40 +0000

    View students scoring below 80

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -1,6 +1,6 @@
 -- need_meeting View for students scoring under 80,
 -- and last meeting longer than a month back
-DROP VIEW IF EXISTS need_meeting
+DROP VIEW IF EXISTS need_meeting;
 CREATE VIEW need_meeting AS
 SELECT name FROM students WHERE score < 80
 AND (students.last_meeting IS NULL OR students.last_meeting < DATE_ADD(NOW(), INTERVAL -1 MONTH));
