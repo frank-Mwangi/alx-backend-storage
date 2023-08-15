a921193a5bc0c8e6ee083d11744e150dab3a0624
frank-Mwangi <kungofrank@gmail.com>
2023-08-15 22:32:49 +0300

    List schools by topic

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Script tthat returns list of school offering
+a specific topic
+"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """List schools offering given topic"""
+    results = mongo_collection.find({"topic": topic})
+    return results
